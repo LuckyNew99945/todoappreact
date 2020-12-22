957bed69fc15e436e7d4018aaab2523fa0d1b008
LuckyNew99945 <luckynew99945@gmail.com>
2020-12-22 10:01:05 +0700

    html structure done

---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './Todo';
+export default function TodoList() {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        <Todo/>
+      </ul>
+    </div>
+  )
+}
