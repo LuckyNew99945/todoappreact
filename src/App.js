38cff16d1d73cc87ca989d006ce97e138aefee4e
LuckyNew99945 <luckynew99945@gmail.com>
2020-12-22 10:08:42 +0700

    make use state hooks for data flow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList'
 function App() {
+
+  //states
+  //input text state for input text for todo
+  const [inputText,setInputText] = useState('');
+  //todos is collection of todo from input text
+  const [todos, setTodos] = useState([]);
+  //set status in select option to determine which todo is shown.default is all
+  const [status,setStatus] = useState('all');
+  //filtered todos is collection of todo that match with status option
+   const [filteredTodos,setFilteredTodos] = useState([]);
   return (
     <div className="App">
       <header>
